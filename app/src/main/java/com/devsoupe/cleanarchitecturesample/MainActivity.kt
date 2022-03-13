f780fffe7e3799e9c3e2f6f8f9cdfe0bb29a2299
devsoupe <luvyum4e@gmail.com>
2022-03-13 21:49:29 +0900

    refactor: editorconfig 적용

--- app/src/main/java/com/devsoupe/cleanarchitecturesample/MainActivity.kt
+++ app/src/main/java/com/devsoupe/cleanarchitecturesample/MainActivity.kt
@@ -13,28 +13,28 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.devsoupe.cleanarchitecturesample.ui.theme.CleanArchitectureSampleTheme
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            CleanArchitectureSampleTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContent {
+      CleanArchitectureSampleTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+          Greeting("Android")
         }
+      }
     }
+  }
 }
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+  Text(text = "Hello $name!")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    CleanArchitectureSampleTheme {
-        Greeting("Android")
-    }
+  CleanArchitectureSampleTheme {
+    Greeting("Android")
+  }
 }
